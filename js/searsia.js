cb76e404d70f39c9040938512dfb6244065a0e70
Djoerd Hiemstra <hiemstra@cs.utwente.nl>
2016-09-26 16:22:56 +0200

    logging, thanks rob!

--- js/searsia.js
+++ js/searsia.js
@@ -34,11 +34,21 @@ var nrResults = 0;   // Total number of results returned
 var page      = 1;   // search result page
 var lang      = document.getElementsByTagName('html')[0].getAttribute('lang');    // used for language-dependent texts
 
+// variables for logging click-through data:
+// the url to log click data, undefined or 0 to disable click logging
+// preferably this url is formatted like '/searsia/clicklogger.php'
+// but 'http://localhost:3000/searsia/clicklogger.php is also possible
+// IMPORTANT: if the former method is used, the url must be in the same
+// domain as the index page to prevent cross site scripting problems!
+var logClickDataUrl = undefined;
+var sendSessionIdentifier = 0; // send anonymous session id with each click
+
 var store = {
 
     hits: [],
     length: 0,
     query: '',
+    ranking: [],
 
     push: function (hit) {
         var i = this.length;
@@ -71,6 +81,21 @@ var store = {
 
     getQuery: function () {
         return this.query;
+    },
+
+    addToRanking: function (resourceId, rank) {
+        this.ranking[rank - 1] = resourceId;
+    },
+
+    removeFromRanking: function (rank) {
+        this.ranking[rank - 1] = "";
+    },
+
+    getRanking: function (rank) {
+        if (rank > 0 && rank < this.ranking.length) {
+            return this.ranking.splice(0, rank);
+        }
+        return this.ranking;
     }
 
 };
@@ -367,20 +392,38 @@ function scoreHit(hit, i, query) {
 }
 
 
-function htmlFullResult(query, hit) {
+/**
+ * creates the onclick function for click log data, which can be inserted
+ * in the html of href elements. This does not create an onclick element
+ * if logClickDataUrl is not specified in the global parameters
+ * @param rank
+ * @param kind
+ * @returns {*}
+ */
+function createOnClickElementforClickThrough(rank, kind) {
+    if (logClickDataUrl) {
+        return ' onclick="logClick(this, \'' + rank + '\', \'' + kind + '\')" ';
+    }
+    return '';
+}
+
+
+function htmlFullResult(query, hit, rank) {
     var result = '',
         title  = hit.title,
         descr  = hit.description,
         url    = hit.url,
         image  = hit.image;
     title = restrict(title, 80);
-    result += '<h4><a href="' + url + '">' + highlightTerms(title, query) + '</a>';
+    result += '<h4><a ' + createOnClickElementforClickThrough(rank, 'html_result_full')
+        + 'href="' + url + '">' + highlightTerms(title, query) + '</a>';
     if (hit.favicon != null) {
         result += '<img src="' + hit.favicon + '" alt="">';
     }
     result += '</h4>';
     if (image != null) {
-        result += '<a href="' + url + '"><img src="' + image + '" /></a>';
+        result += '<a ' + createOnClickElementforClickThrough(rank, 'html_result_full')
+            + 'href="' + url + '"><img src="' + image + '" /></a>';
     }
     result += '<p>';
     if (descr == null) { descr = hit.title; }
@@ -389,18 +432,26 @@ function htmlFullResult(query, hit) {
     } else {
         result += '&#151;';
     }
-    result += '<br><a href="' + url + '">' + highlightTerms(restrict(url, 90), query) + '</a></p>';
+    result += '<br><a ' + createOnClickElementforClickThrough(rank, 'html_result_full')
+        + 'href="' + url + '">' + highlightTerms(restrict(url, 90), query) + '</a></p>';
     return result;
 }
 
-
-function htmlSuggestionResult(resource, hit) {
+/**
+ * Returns suggestions like 'related searches' and 'did you mean'
+ * @param resource
+ * @param hit
+ * @param rank
+ * @returns {string}
+ */
+function htmlSuggestionResult(resource, hit, rank) {
     var result = '',
         title  = hit.title,
         url    = hit.url;
     title = restrict(title, 80);
     result += '<h4>' + resource.name;
-    result += ' <a href="' + url + '"><b>' + title + '</b></a></h4>';
+    result += ' <a ' + createOnClickElementforClickThrough(rank, 'suggested_result')
+        + 'href="' + url + '"><b>' + title + '</b></a></h4>';
     return result;
 }
 
@@ -454,14 +505,14 @@ function moreResults(event) {
     for (i = 0; i < maxi; i += 1) {
         hit = store.shift();
         result += '<div class="search-result">';
-        result += htmlFullResult(query, hit);
+        //TODO add ranking for this result (?)
+        result += htmlFullResult(query, hit, -1);
         result += '</div>';
     }
     $('#searsia-results-4').append(result); // there are three divs for results, 1=top, 2=subtop, 3=rest, 4=more
     if (store.length <= 0) {
         $('#searsia-alert-bottom').html(noMoreResultsText());
     }
-    // $wh.fireLayoutChangeEvent(document.getElementById("searsiasearch"));
 }
 
 
@@ -605,7 +656,7 @@ function removeTooOldResults(data) {
  * max length 220 characters, restricting the size of the
  * title and description. Title at least 80 characters.
  */
-function htmlSubResultWeb(query, hit) {
+function htmlSubResultWeb(query, hit, rank) {
     var title  = hit.title,
         descr  = hit.description,
         url    = hit.url,
@@ -628,10 +679,12 @@ function htmlSubResultWeb(query, hit) {
     }
     result += '<div class="sub-result">';
     if (image != null) {
-        result += '<a href="' + url + '"><img src="' + image + '"/></a>';
+        result += '<a ' + createOnClickElementforClickThrough(rank, 'subresult_html_web')
+            + 'href="' + url + '"><img src="' + image + '"/></a>';
     }
-    result += '<p><a href="' + url + '">' + highlightTerms(title, query) + '</a> ';
-    if(tLength < 40 && dLength < 40) {
+    result += '<p><a ' + createOnClickElementforClickThrough(rank, 'subresult_html_web')
+        + 'href="' + url + '">' + highlightTerms(title, query) + '</a> ';
+    if (tLength < 40 && dLength < 40) {
         result += '<br>';
     }
     if (descr != null) {
@@ -642,7 +695,7 @@ function htmlSubResultWeb(query, hit) {
 }
 
 
-function htmlSubResultWebFull(query, hit) { // duplicate code with htmlSubResultWeb()
+function htmlSubResultWebFull(query, hit, rank) { // duplicate code with htmlSubResultWeb()
     var title  = hit.title,
         descr  = hit.description,
         url    = hit.url,
@@ -653,22 +706,25 @@ function htmlSubResultWebFull(query, hit) { // duplicate code with htmlSubResult
     maxsnip -= title.length;
     result += '<div class="sub-result">';
     if (image != null) {
-        result += '<a href="' + url + '"><img src="' + image + '"/></a>';
+        result += '<a ' + createOnClickElementforClickThrough(rank, 'subresult_web_full')
+            + 'href="' + url + '"><img src="' + image + '"/></a>';
     }
-    result += '<div class="descr"><a href="' + url + '">' + highlightTerms(title, query) + '</a> ';
+    result += '<div class="descr"><a ' + createOnClickElementforClickThrough(rank, 'subresult_web_full')
+        + 'href="' + url + '">' + highlightTerms(title, query) + '</a> ';
     if (descr != null) {
         result += highlightTerms(restrict(descr, maxsnip), query);
     }
     result += '</div>';
     if (url != null) {
-        result += '<div class="url"><a href="' + url + '">' + highlightTerms(url, query) + '</a></div>';
+        result += '<div class="url"><a ' + createOnClickElementforClickThrough(rank, 'subresult_web_full')
+            + 'href="' + url + '">' + highlightTerms(url, query) + '</a></div>';
     }
     result += '</div>';
     return result;
 }
 
 
-function htmlSubResultSmall(query, hit) {
+function htmlSubResultSmall(query, hit, rank) {
     var title  = hit.title,
         descr  = hit.description,
         url    = hit.url,
@@ -677,7 +733,8 @@ function htmlSubResultSmall(query, hit) {
     title = restrict(title, 80);
     maxsnip -= title.length;
     result += '<div class="sub-result-small">';
-    result += '<p><a href="' + url + '">' + highlightTerms(title, query) + '</a> ';
+    result += '<p><a ' + createOnClickElementforClickThrough(rank, 'subresult_html_small')
+        + 'href="' + url + '">' + highlightTerms(title, query) + '</a> ';
     if (descr != null && maxsnip > 40) {
         result += highlightTerms(restrict(descr, maxsnip), query);
     }
@@ -686,13 +743,14 @@ function htmlSubResultSmall(query, hit) {
 }
 
 
-function htmlSubResultImage(hit) {
+function htmlSubResultImage(hit, rank) {
     var title  = hit.title,
         url    = hit.url,
         image  = hit.image,
         result = '';
     title = restrict(title, 80);
-    result += '<a href="' + url + '"><img class="sub-image" src="' + image + '" alt="[image]" title="' + title + '"/></a>\n';
+    result += '<a ' + createOnClickElementforClickThrough(rank, 'subresult_image')
+        + 'href="' + url + '"><img class="sub-image" src="' + image + '" alt="[image]" title="' + title + '"/></a>\n';
     return result;
 }
 
@@ -714,7 +772,7 @@ function addToStore(data, begin, end) {
 }
 
 
-function htmlSubResults(query, data) {
+function htmlSubResults(query, data, rank) {
     var i, maxr,
         MAXX = 4,
         result = '<div>',
@@ -726,13 +784,13 @@ function htmlSubResults(query, data) {
     if (count > MAXX) { count = MAXX; }
     for (i = 0; i < count; i += 1) {
         if (resource.type === 'small') {
-            result += htmlSubResultSmall(query, data.hits[i]);
+            result += htmlSubResultSmall(query, data.hits[i], rank);
         } else if (resource.type === 'images') {
-            result += htmlSubResultImage(data.hits[i]);
+            result += htmlSubResultImage(data.hits[i], rank);
         } else if (resource.type === 'full') {
-            result += htmlSubResultWebFull(query, data.hits[i]);
+            result += htmlSubResultWebFull(query, data.hits[i], rank);
         } else {
-            result += htmlSubResultWeb(query, data.hits[i]);
+            result += htmlSubResultWeb(query, data.hits[i], rank);
         }
     }
     result += '</div>';
@@ -743,7 +801,7 @@ function htmlSubResults(query, data) {
 }
 
 
-function htmlResource(query, resource, printQuery) {
+function htmlResource(query, resource, printQuery, rank) {
     var url, title,
         result = '<h4>';
     if (resource.urltemplate != null) {
@@ -754,7 +812,8 @@ function htmlResource(query, resource, printQuery) {
         } else {
             url = fillUrlTemplate(resource.urltemplate, '', '');
         }
-        result += '<a href="' + url + '">';
+        result += '<a ' + createOnClickElementforClickThrough(rank, 'html_resource_header')
+            + 'href="' + url + '">';
         title = restrict(title, 80);
         result += highlightTerms(title, query) + '</a>';
         if (resource.favicon != null) {
@@ -769,17 +828,18 @@ function htmlResource(query, resource, printQuery) {
         result += highlightTerms(restrict(resource.summary, 90), query) + '<br>';
     }
     if (url != null) {
-        result += '<a href="' + url + '">' + highlightTerms(restrict(url, 90), query) + '</a>';
+        result += '<a ' + createOnClickElementforClickThrough(rank, 'html_resource_header')
+            + 'href="' + url + '">' + highlightTerms(restrict(url, 90), query) + '</a>';
     }
     result += '</p>';
     return result;
 }
 
 
-function printSingleResult(query, hit, where) {
-    var result;
+function printSingleResult(query, hit, rank) {
+    var result, where = rank;
     result = '<div class="search-result">';
-    result += htmlFullResult(query, hit);
+    result += htmlFullResult(query, hit, rank);
     result += '</div>';
     if (where < 1) { where = 1; }
     if (where > 4) { where = 4; }
@@ -795,18 +855,21 @@ function printAggregatedResults(query, data, rank, printQuery) {
         result += '<div class="search-result">';
         if (count === 1) {
             if (data.hits[0].tags != null && data.hits[0].tags.indexOf('suggestion') !== -1) {
-                result += htmlSuggestionResult(resource, data.hits[0]);
+                result += htmlSuggestionResult(resource, data.hits[0], rank);
             } else {
-                result += htmlFullResult(query, data.hits[0]);
+                result += htmlFullResult(query, data.hits[0], rank);
             }
         } else {
-            result += htmlResource(query, resource, printQuery);
-            result += htmlSubResults(query, data);
+            result += htmlResource(query, resource, printQuery, rank);
+            result += htmlSubResults(query, data, rank);
         }
         result += '</div>';
         if (rank < 1) { rank = 1; }
         if (rank > 4) { rank = 4; }
         $('#searsia-results-' + rank).append(result); // there are four divs for results, 1=top, 2=subtop, 3=rest, 4=more.
+    } else {
+        //Remove this resource from the ranking because it is not shown to the user
+        store.removeFromRanking(rank);
     }
 }
 
@@ -818,7 +881,7 @@ function printNormalResults(query, data, rank) {
     if (count > MAXX) { count = MAXX; }
     for (i = 0; i < count; i += 1) {
         result = '<div class="search-result">';
-        result += htmlFullResult(query, data.hits[i]);
+        result += htmlFullResult(query, data.hits[i], rank);
         result += '</div>';
         where = rank + i;
         if (where < 1) { where = 1; }
@@ -855,7 +918,6 @@ function printResults(query, data, rank, olddata) {
     if (pending <= 0) {
         checkEmpty();
     }
-    // $wh.fireLayoutChangeEvent(document.getElementById("searsiasearch"));
 }
 
 
@@ -887,6 +949,7 @@ function queryResources(query, data) {
     hits = data.hits;
     while (i < hits.length) {
         rid = hits[i].rid;
+        store.addToRanking(rid, rank); // store the ranking of this resource
         if (rid == null) { // a result that is not from another resource
             if (data.resource == null || data.resource.rerank == null || scoreHit(hits[i], 0, query) > 0.0) { // TODO: real reranking
                 printSingleResult(query, hits[i], rank);
@@ -923,7 +986,6 @@ function queryResources(query, data) {
     if (pending < 1) {
         checkEmpty();
     }
-    // $wh.fireLayoutChangeEvent(document.getElementById("searchresults"));
 }
 
 
@@ -981,6 +1043,123 @@ function initSearsiaClient(data) {
     placeIcon(data);
     placeName(data);
     storeResources(data);
+}
+
+
+/**
+ * set cookie value
+ * @param cname name of the cookie
+ * @param cvalue value of the cookie
+ * @param exdays days before the cookie expires
+ */
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date(),
+        expires;
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
+/**
+ * get a cookie value
+ * @param cname the name of the cookie
+ * @returns {*}
+ */
+function getCookie(cname) {
+    var name = cname + "=",
+        ca = document.cookie.split(';'),
+        i,
+        c;
+    for (i = 0; i < ca.length; i += 1) {
+        c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+/**
+ * Generates a random identifier compliant with the uuid(v4) spec.
+ * The randomness of this number is based on Math.random(), which might not
+ * be a guaranteed RNG.
+ * @returns {string} the uuid string
+ */
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
+        function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+}
+
+
+/**
+ * Returns an anonymous identifier for the user session, creates a session
+ * if a session does not exist This session identifier is generated and
+ * stored client-side. This is nog a server-side session.
+ */
+function getOrSetSessionIdentifier() {
+    var sessionId = getCookie('sessionId');
+    if (!sessionId) {
+        sessionId = generateUUID();
+        setCookie('sessionId', sessionId, 1);
+    }
+    return sessionId;
+}
+
+
+/**
+ * converts a standard url to a url suitable for clicklogging
+ * Does not convert if click through data is disabled
+ * This can be done by replacing the contents of the logClickDataUrl
+ * variable with 0 or undefined
+ */
+function convertUrlForClickThroughData(url, rank, kind) {
+    if (logClickDataUrl) {
+        //replace &amp; with normal & for encoding the uri component
+        url = url.replace(/&amp;/g, "&");
+        url = encodeURIComponent(url);
+        url = logClickDataUrl + '?url=' + url;
+
+        url += '&query=' + store.getQuery();
+
+        if (rank) {
+            url += '&rank=' + rank;
+        }
+        if (kind) {
+            url += '&kind=' + kind;
+        }
+        if (sendSessionIdentifier) {
+            url += '&sessionId=' + getOrSetSessionIdentifier();
+        }
+        url += '&ordering=' + store.getRanking(rank).toString();
+    }
+    return url;
+}
+
+
+/**
+ * This function is used although editors might show it as unused, this
+ * function is only called from generated html. This functions logs the
+ * click data and continues normal forwarding operations for the user.
+ * @param element the element that is clicked on
+ * @param rank the rank of the clicked element
+ * @param kind the kind of link that is clicked on
+ */
+function logClick(element, rank, kind) {
+    if (logClickDataUrl) {
+        //TODO make post call
+        $.ajax({
+            type: "GET",
+            url: convertUrlForClickThroughData($(element).attr('href'), rank, kind)
+        });
+    }
 }
 
 
